1e3fcb6b81f7f6cc919ede5bd72c9c76149f9300
Kahuã Dos Santos Dias <kahuasantos1@gmail.com>
2026-02-19 19:53:18 -0300

    Update Arrays.java

--- Estrutura_de_dados1/Arrays.java
+++ Estrutura_de_dados1/Arrays.java
@@ -1,1 +1,24 @@
+os arrays umdimensionais são os que possuem apenas um índice  para acessar seu conteúdo eles
+  são declarados da seguinte forma:
 
+eles são tipo de dado [] nome do array = new tipo-de-dado [quantidade]
+
+  tipo de dado -> pode ser qualquer tipo de variável 
+  nome do array -> um nome válido; as mesmas regras para nomes de variáveis 
+  Quantidade -> quantidade de elementos que o array pode manipular 
+
+  exemplos: 
+ int [] numeros, = new int [10] Cria uma array  com o nome numeros que contém 10 elementos do 
+  tipo int e seu índice vania de 0 a 9. 
+
+  doble [] preços = new doble [s]; Cria um array com o nome precos que cotém 5 elementosdo tipo
+  doble e seu indice varia de 0 a 4. 
+
+  char [] sexo = char [2]; cria array com o nome sexo que contém 2 elementos do tio char e seu
+  índice varia entre 0 e 1. 
+  
+  boolean [] opcoes = new boocean[2]; cria um array com o nome opcoes que contem 2 elementos do
+  ripo boolean e seu índice varia entre
+  0 e 1.
+
+  string [] meses = new string [12]; cria um array com o nome meses que contém 12 elementos do tipo string e seu índice varia de 0 a 11. 
